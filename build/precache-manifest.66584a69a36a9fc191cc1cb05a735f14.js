a3256f53229fb7d00d59e510513d06cdde2686e0
Anna Beagley <beagley.dev@gmail.com>
2019-04-24 11:23:06 -0600

    second commit

---
+++ build/precache-manifest.66584a69a36a9fc191cc1cb05a735f14.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3049287ff31aa4d09cd8663ce627d5ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bc79b8e56824cc1b352",
+    "url": "/static/js/2.762a2f44.chunk.js"
+  },
+  {
+    "revision": "3056cb00d74bbe93163e",
+    "url": "/static/js/main.a3c16205.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
