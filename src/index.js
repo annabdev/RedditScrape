2e5a5d3338fa4f938d9c065fe71d3872ffd97ad5
Anna Beagley <beagley.dev@gmail.com>
2019-04-24 11:19:38 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+//axios API makes the HTTP requests from the browser
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Reddit extends React.Component {
+    state = {
+        posts: []
+    };
+
+    componentDidMount() {
+        axios.get(`https://www.reddit.com/r/reactjs.json`).then(res => {
+            const posts = res.data.data.children.map(obj =>
+                obj.data);
+                this.setState({ posts });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>/r/reactjs</h1>
+                <ul>
+                    {this.state.posts.map(post => {
+                        return <li key={post.id}>{post.title}</li>;
+                    })}
+                </ul>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Reddit />, document.getElementById("root"));
